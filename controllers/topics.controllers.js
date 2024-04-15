703464443779d20946334184db327e44225b4709
Kim Lee <kimberleylee1990@gmail.com>
2024-04-15 13:52:01 +0100

    Complete task3 get api endpoint

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,13 @@
 const { fetchData } = require("../models/topics.models.js");
 
 const getTopics = (req, res, next) => {
-  fetchData().then((topics) => {
-    res.status(200).send({ topics });
-  });
+  fetchData()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 module.exports = { getTopics };
