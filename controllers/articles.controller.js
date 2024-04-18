17e2627ddd0a19297fdb1e9bb32a062c3cbaf563
Kim Lee <kimberleylee1990@gmail.com>
2024-04-18 17:02:19 +0100

    Create GET api/articles with sorting queries (#16)

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -6,8 +6,8 @@ const {
 const { checkTopicExists } = require("../models/topics.model");
 
 const getArticles = (req, res, next) => {
-  const { topic } = req.query;
-  Promise.all([fetchArticles(topic), checkTopicExists(topic)])
+  const { topic, sort_by, order } = req.query;
+  Promise.all([fetchArticles(topic, sort_by, order), checkTopicExists(topic)])
     .then(([articles]) => {
       res.status(200).send({ articles });
     })
