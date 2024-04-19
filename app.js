56b962d72cb6f3f0a8a15dc02f92eb6e787b1ac0
Kim Lee <kimberleylee1990@gmail.com>
2024-04-19 11:21:15 +0100

    Created routers for /articles, /comments and /users

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
 
-const {
-  getArticleById,
-  getArticles,
-  patchArticleById,
-} = require("./controllers/articles.controller");
-const {
-  getComments,
-  postComment,
-  deleteComment,
-} = require("./controllers/comments.controller");
-const { getUsers } = require("./controllers/users.controller");
-
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticleById);
-app.patch("/api/articles/:article_id", patchArticleById);
-
-app.get("/api/articles/:article_id/comments", getComments);
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ message: "Not found!" });
