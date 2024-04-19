e48d0c4557c4cb5cd45c9725384ba189d5da8525
Kim Lee <kimberleylee1990@gmail.com>
2024-04-19 11:57:38 +0100

    Created GET /api/users/:username 200 and 404 test

--- models/users.model.js
+++ models/users.model.js
@@ -4,4 +4,14 @@ const fetchUsers = () => {
   return db.query(`SELECT * FROM users;`).then(({ rows }) => rows);
 };
 
-module.exports = { fetchUsers };
+const fetchUserByUsername = (username) => {
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+      return rows.length
+        ? rows[0]
+        : Promise.reject({ status: 404, message: "Not found" });
+    });
+};
+
+module.exports = { fetchUsers, fetchUserByUsername };
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,6 +1,11 @@
 const usersRouter = require("express").Router();
-const { getUsers } = require("../controllers/users.controller");
+const {
+  getUsers,
+  getUserByUsername,
+} = require("../controllers/users.controller");
 
 usersRouter.route("/").get(getUsers);
+
+usersRouter.route("/:username").get(getUserByUsername);
 
 module.exports = usersRouter;
